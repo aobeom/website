4228d33404a02640b185873b8587409da8bcf8d3
aobeom <csmcc2@hotmail.com>
2018-06-19 22:46:22 +0800

    update2

--- apps/__init__.py
+++ apps/__init__.py
@@ -5,7 +5,7 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 from flask import Flask
 from flask_login import LoginManager, login_manager
 from get_config import get_db_conf
-from flask_cors import CORS
+# from flask_cors import CORS
 # mysql
 # from flask_sqlalchemy import SQLAlchemy
 
